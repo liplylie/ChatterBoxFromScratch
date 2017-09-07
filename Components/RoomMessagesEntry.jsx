85116e8c5bea6fa7d4b21d8fb5f7ccc78dd2dd3c
Jordan Daniels <decentpianist@gmail.com>
2017-09-06 22:57:34 -0700

    clicking on room name console logs the name of the room

--- Components/RoomMessagesEntry.jsx
+++ Components/RoomMessagesEntry.jsx
@@ -1,14 +1,16 @@
 var RoomMessagesEntry = (props) => {
 
 
-
+	var textInput = function(event){
+	    console.log('Input is ' + event.target.innerHTML) 
+	  }
 
 
   console.log(props, "RoomMessagesEntry props yoo");
   return (
   <div className="video-list-entry media">
     <div className="media-body">
-      <div className="video-list-entry-detail"><span className= "room" >{props.room}</span> </div>
+      <div className="video-list-entry-detail"><span className= "room" onClick={textInput.bind(this)} >{props.room}</span> </div>
     </div>
   </div>
 ); 
